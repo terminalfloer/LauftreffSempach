87e38d8186ddd95d0f9876660255ec00527c0973
floer <florian.sennhauser@bluewin.ch>
2023-06-28 09:21:22 +0200

    Implementation of slideshow

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,3 +2,24 @@ function toggleSidebar() {
     var sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('open');
   }
+
+var slides = document.getElementsByClassName('slide');
+var currentSlideIndex = 0;
+
+function showSlide(index) {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].classList.remove('active');
+  }
+  slides[index].classList.add('active');
+}
+
+function nextSlide() {
+  currentSlideIndex++;
+  if (currentSlideIndex >= slides.length) {
+    currentSlideIndex = 0;
+  }
+  showSlide(currentSlideIndex);
+}
+
+// Automatischer Wechsel der Bilder alle 3 Sekunden
+setInterval(nextSlide, 3000);
